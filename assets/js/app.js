01e5dcf58a9275c1da948ab02ac9dd472195faa0
Adel Mokhtari <adel.mokhtari@universite-paris-saclay.fr>
2026-04-04 18:16:16 +0200

    gestion des formulaires

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,12 +34,113 @@ $(document).ready(function() {
         $('#bloc-mdp-oublie').slideToggle(300);
     });
 
+    // ===== FORMULAIRE CONNEXION =====
+    $('#form-connexion').on('submit', function(e) {
+        e.preventDefault();
+        var formData = $(this).serialize() + '&action=connexion';
+
+        $.post('index.php', formData, function(res) {
+            if (res.success) {
+                $('#popup-connexion').removeClass('active');
+                $('#nav-connexion').addClass('d-none');
+                $('#nav-dashboard').removeClass('d-none');
+                $('#nav-deconnexion').removeClass('d-none');
+                $('#bloc-info-client').hide();
+                // Remplir les infos perso dans le dashboard
+                $('#info-nom').val(res.nom);
+                $('#info-prenom').val(res.prenom);
+                $('#info-email').val(res.email);
+                alert('Bienvenue ' + res.prenom + ' ' + res.nom + ' !');
+            } else {
+                alert(res.error);
+            }
+        }, 'json');
+    });
+
+    // ===== DECONNEXION =====
+    $('#btn-deconnexion').on('click', function(e) {
+        e.preventDefault();
+        $.post('index.php', { action: 'deconnexion' }, function(res) {
+            if (res.success) {
+                $('#nav-connexion').removeClass('d-none');
+                $('#nav-dashboard').addClass('d-none');
+                $('#nav-deconnexion').addClass('d-none');
+                $('#bloc-info-client').show();
+            }
+        }, 'json');
+    });
+
+    // ===== FORMULAIRE MOT DE PASSE OUBLIE =====
+    $('#form-mdp-oublie').on('submit', function(e) {
+        e.preventDefault();
+        var formData = $(this).serialize() + '&action=mdp_oublie';
+
+        $.post('index.php', formData, function(res) {
+            alert(res.message || res.error);
+            $('#bloc-mdp-oublie').slideUp(300);
+        }, 'json');
+    });
+
+    // ===== FORMULAIRE RESERVATION =====
+    $('#form-reservation').on('submit', function(e) {
+        e.preventDefault();
+        var formData = $(this).serialize() + '&action=reservation';
+
+        $.post('index.php', formData, function(res) {
+            if (res.success) {
+                alert(res.message);
+                $('#popup-reservation').removeClass('active');
+                $('#form-reservation')[0].reset();
+            } else {
+                alert(res.error);
+            }
+        }, 'json');
+    });
+
+    // ===== CARROUSEL =====
+    $('.carrousel-next').on('click', function() {
+        var track = $('#' + $(this).data('cible'));
+        var item = track.find('.carrousel-item').first();
+        var step = item.outerWidth() + 20;
+        track.scrollLeft(track.scrollLeft() + step * 3);
+        setTimeout(function() { majFleches(track); }, 550);
+    });
+
+    $('.carrousel-prev').on('click', function() {
+        var track = $('#' + $(this).data('cible'));
+        var item = track.find('.carrousel-item').first();
+        var step = item.outerWidth() + 20;
+        track.scrollLeft(track.scrollLeft() - step * 3);
+        setTimeout(function() { majFleches(track); }, 550);
+    });
+
+    // ===== ONGLETS DASHBOARD =====
+    $('.dash-tab').on('click', function() {
+        $('.dash-tab').removeClass('active');
+        $(this).addClass('active');
+        $('.dash-panel').removeClass('active');
+        $('#' + $(this).data('tab')).addClass('active');
+    });
+
     // ===== CHARGEMENT INITIAL (AJAX) =====
     chargerChambresAccueil();
     chargerActivitesAccueil();
     chargerPrestationsAccueil();
+    chargerChambresFormulaire();
+    chargerActivitesFormulaire();
 
 });
+
+
+// ===== CARROUSEL - MAJ FLECHES =====
+function majFleches(track) {
+    var wrapper = track.closest('.carrousel-wrapper');
+    var scrollLeft = track.scrollLeft();
+    var maxScroll = track[0].scrollWidth - track[0].clientWidth;
+
+    wrapper.find('.carrousel-prev').toggleClass('carrousel-disabled', scrollLeft <= 0);
+    wrapper.find('.carrousel-next').toggleClass('carrousel-disabled', scrollLeft >= maxScroll - 1);
+}
 
 
 // ===== FONCTIONS AJAX =====
@@ -53,19 +154,19 @@ function chargerChambresAccueil() {
     .done(function(chambres) {
         var html = '';
         $.each(chambres, function(i, ch) {
-            html += '<div class="col-md-4 mb-4">';
+            html += '<div class="carrousel-item">';
             html += '  <div class="glass-card">';
-            html += '    <h5>' + ch.nom_chambre + '</h5>';
-            html += '    <p class="text-muted">' + ch.type_chambre + ' - ' + ch.capacite + ' pers.</p>';
+            html += '    <h5>' + ch.nom + '</h5>';
+            html += '    <p class="text-muted">' + ch.type + ' - ' + ch.capacite + ' pers.</p>';
             html += '    <p class="prix">' + ch.prix_nuit + ' &euro; / nuit</p>';
             html += '  </div>';
             html += '</div>';
         });
         $('#liste-chambres').html(html);
+        majFleches($('#liste-chambres'));
     })
     .fail(function() {
-        var msg = '<p class="text-muted">Les chambres seront disponibles prochainement.</p>';
-        $('#liste-chambres').html(msg);
+        $('#liste-chambres').html('<p class="text-muted">Les chambres seront disponibles prochainement.</p>');
     });
 }
 
@@ -73,12 +174,11 @@ function chargerActivitesAccueil() {
     $.ajax({
         url: 'api/activites.php',
         method: 'GET',
-        dataType: 'json',
+        dataType: 'json'
     })
     .done(function(activites) {
         var html = '';
         $.each(activites, function(i, act) {
-            // Badge couleur selon le type d'activite
             var badgeClass = 'bg-secondary';
             if (act.type) {
                 var t = act.type.toLowerCase();
@@ -89,7 +189,7 @@ function chargerActivitesAccueil() {
                 else if (t === 'individuel') badgeClass = 'badge-individuel';
             }
 
-            html += '<div class="col-md-4 mb-4">';
+            html += '<div class="carrousel-item">';
             html += '  <div class="glass-card">';
             html += '    <h5>' + act.nom + '</h5>';
             html += '    <span class="badge ' + badgeClass + '">' + (act.type || '') + '</span>';
@@ -99,10 +199,10 @@ function chargerActivitesAccueil() {
             html += '</div>';
         });
         $('#liste-activites').html(html);
+        majFleches($('#liste-activites'));
     })
     .fail(function() {
-        var msg = '<p class="text-muted">Les activites seront disponibles prochainement.</p>';
-        $('#liste-activites').html(msg);
+        $('#liste-activites').html('<p class="text-muted">Les activites seront disponibles prochainement.</p>');
     });
 }
 
@@ -110,12 +210,12 @@ function chargerPrestationsAccueil() {
     $.ajax({
         url: 'api/prestations.php',
         method: 'GET',
-        dataType: 'json',
+        dataType: 'json'
     })
     .done(function(prestations) {
         var html = '';
         $.each(prestations, function(i, p) {
-            html += '<div class="col-md-4 mb-4">';
+            html += '<div class="carrousel-item">';
             html += '  <div class="glass-card">';
             html += '    <h5>' + p.nom + '</h5>';
             html += '    <p class="text-muted">' + (p.description || '') + '</p>';
@@ -124,9 +224,51 @@ function chargerPrestationsAccueil() {
             html += '</div>';
         });
         $('#liste-prestations').html(html);
+        majFleches($('#liste-prestations'));
     })
     .fail(function() {
-        var msg = '<p class="text-muted">Les prestations seront disponibles prochainement.</p>';
-        $('#liste-prestations').html(msg);
+        $('#liste-prestations').html('<p class="text-muted">Les prestations seront disponibles prochainement.</p>');
+    });
+}
+
+
+// ===== REMPLISSAGE FORMULAIRE RESERVATION =====
+
+function chargerChambresFormulaire() {
+    $.ajax({
+        url: 'api/chambres.php',
+        method: 'GET',
+        dataType: 'json'
+    })
+    .done(function(chambres) {
+        var html = '<option value="" selected disabled>Choisir une chambre...</option>';
+        $.each(chambres, function(i, ch) {
+            html += '<option value="' + ch.id + '">';
+            html += ch.nom + ' (' + ch.type + ') - ' + ch.capacite + ' pers. - ' + ch.prix_nuit + ' &euro;/nuit';
+            html += '</option>';
+        });
+        $('#res-chambre').html(html);
+    });
+}
+
+function chargerActivitesFormulaire() {
+    $.ajax({
+        url: 'api/activites.php',
+        method: 'GET',
+        dataType: 'json'
+    })
+    .done(function(activites) {
+        var html = '';
+        $.each(activites, function(i, act) {
+            html += '<div class="col-md-6 mb-2">';
+            html += '  <div class="form-check">';
+            html += '    <input class="form-check-input" type="checkbox" name="activites[]" value="' + act.id + '" id="act-' + act.id + '">';
+            html += '    <label class="form-check-label" for="act-' + act.id + '">';
+            html += '      ' + act.nom + ' <small class="text-muted">(' + act.prix + ' &euro;)</small>';
+            html += '    </label>';
+            html += '  </div>';
+            html += '</div>';
+        });
+        $('#res-activites').html(html);
     });
 }
