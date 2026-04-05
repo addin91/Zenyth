d8c8597ca42a8ff3db72c6d81757b41c97ad44a1
Addin <abdelrahmenbentahar@gmail.com>
2026-04-05 19:21:54 +0200

    Correction des erreurs service Mail + ajout info email

--- services/MailService.php
+++ services/MailService.php
@@ -1,60 +1,65 @@
 <?php 
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 class MailService{
     private $mail;
 
     public function __construct(){
-        $this->pdo = new PHPMailer(true);
-        initialisation();
+        $this->mail = new PHPMailer(true);
+        $this->initialisation();
     }
 
     private function initialisation(){
        try {
             // Configuration SMTP
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = '';
-            $mail->Password = '';
-            $mail->SMTPSecure = 'tls';
-            $mail->Port = 587;
+            $this->mail->isSMTP();
+            $this->mail->Host = 'smtp.gmail.com';
+            $this->mail->SMTPAuth = true;
+            $this->mail->Username = 'zenyth.tourisme@gmail.com';
+            $this->mail->Password = 'jkws qgzh rdpx jfqk';
+            $this->mail->SMTPSecure = 'tls';
+            $this->mail->Port = 587;
 
         } catch (Exception $e) {
-            echo "Erreur : {$mail->ErrorInfo}";
+            echo "Erreur : {$this->mail->ErrorInfo}";
         }
     }
 
     public function envoiePassword($destinataire, $password){
         try{
             // Expéditeur et destinataire
-            $mail->setFrom('tonemail@gmail.com', 'Mon App');
-            $mail->addAddress($destinataire);
+            $this->mail->setFrom('tonemail@gmail.com', 'Mon App');
+            $this->mail->addAddress($destinataire);
 
             // Contenu
-            $mail->isHTML(true);
-            $mail->Subject = 'Votre mot de passe';
-            $mail->Body = 'Voici votre mot de passe : <b>'. $password .'</b>';
+            $this->mail->isHTML(true);
+            $this->mail->Subject = 'Votre mot de passe';
+            $this->mail->Body = 'Voici votre mot de passe : <b>'. $password .'</b>';
 
-            $mail->send();
+            $this->mail->send();
         } catch (Exception $e) {
-            echo "Erreur : {$mail->ErrorInfo}";
+            echo "Erreur : {$this->mail->ErrorInfo}";
         }
     }
 
     public function envoieMail($destinataire, $objet, $message, $isHTML){
         try{    
             // Expéditeur et destinataire
-            $mail->setFrom('tonemail@gmail.com', 'Mon App');
-            $mail->addAddress($destinataire);
+            $this->mail->setFrom('tonemail@gmail.com', 'Mon App');
+            $this->mail->addAddress($destinataire);
 
             // Contenu
-            $mail->isHTML($isHTML);
-            $mail->Subject = $objet;
-            $mail->Body = $message;
+            $this->mail->isHTML($isHTML);
+            $this->mail->Subject = $objet;
+            $this->mail->Body = $message;
 
-            $mail->send();
+            $this->mail->send();
         } catch (Exception $e) {
-            echo "Erreur : {$mail->ErrorInfo}";
+            echo "Erreur : {$this->mail->ErrorInfo}";
         }
     }
 }
